f66751e53a69d5bf4e0e726b5cf4d9b006bdb817
Gilyoung Kim <roadzero@kodebox.io>
2018-06-09 16:18:10 +0900

    Update header menu style.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -7,6 +7,27 @@ import './../styles/main.scss'
 import 'bootstrap'
 import 'babel-polyfill'
 
+import $ from 'jquery'
+
 if (process.env.NODE_ENV !== 'production') {
   require('./../index.pug')
 }
+
+$('nav').find('a').click(function (e) {
+  e.preventDefault()
+  var section = $(this).attr('href')
+  $('html, body').animate({
+    scrollTop: $(section).offset().top
+  })
+})
+
+$(window).on('scroll', function () {
+  $('.scroll-target').each(function () {
+    if ($(window).scrollTop() >= $(this).offset().top) {
+      var id = $(this).attr('id')
+      $('nav li').removeClass('active')
+      $('nav li a').blur()
+      $('nav a[href="#' + id + '"]').parent().addClass('active')
+    }
+  })
+})
